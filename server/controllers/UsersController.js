88dfd423b3a072ba4d6a661cde1e394f1b04f653
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-17 11:35:38 +0700

    TDD Testing init

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -1,111 +1,90 @@
-var UsersModel = require('../models/UsersModel.js');
+var UsersModel = require('../models/UsersModel.js')
 
-/**
- * UsersController.js
- *
- * @description :: Server-side logic for managing Userss.
- */
 module.exports = {
+  list: function (req, res) {
+    UsersModel.find(function (err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Users.',
+          error: err
+        })
+      }
+      return res.json(Users)
+    })
+  },
 
-    /**
-     * UsersController.list()
-     */
-    list: function (req, res) {
-        UsersModel.find(function (err, Userss) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Users.',
-                    error: err
-                });
-            }
-            return res.json(Userss);
-        });
-    },
+  show: function (req, res) {
+    var id = req.params.id
+    UsersModel.findOne({_id: id}, function (err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Users.',
+          error: err
+        })
+      }
+      if (!Users) {
+        return res.status(404).json({
+          message: 'No such Users'
+        })
+      }
+      return res.json(Users)
+    })
+  },
 
-    /**
-     * UsersController.show()
-     */
-    show: function (req, res) {
-        var id = req.params.id;
-        UsersModel.findOne({_id: id}, function (err, Users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Users.',
-                    error: err
-                });
-            }
-            if (!Users) {
-                return res.status(404).json({
-                    message: 'No such Users'
-                });
-            }
-            return res.json(Users);
-        });
-    },
+  create: function (req, res) {
+    var Users = new UsersModel({      name: req.body.name,      hunger: req.body.hunger,      thirst: req.body.thirst,      fatigue: req.body.fatigue,      awesomeness: req.body.awesomeness
+    })
 
-    /**
-     * UsersController.create()
-     */
-    create: function (req, res) {
-        var Users = new UsersModel({			name : req.body.name,			hunger : req.body.hunger,			thirst : req.body.thirst,			fatigue : req.body.fatigue,			awesomeness : req.body.awesomeness
-        });
+    Users.save(function (err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when creating Users',
+          error: err
+        })
+      }
+      return res.status(201).json(Users)
+    })
+  },
 
-        Users.save(function (err, Users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating Users',
-                    error: err
-                });
-            }
-            return res.status(201).json(Users);
-        });
-    },
+  update: function (req, res) {
+    var id = req.params.id
+    UsersModel.findOne({_id: id}, function (err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Users',
+          error: err
+        })
+      }
+      if (!Users) {
+        return res.status(404).json({
+          message: 'No such Users'
+        })
+      }
 
-    /**
-     * UsersController.update()
-     */
-    update: function (req, res) {
-        var id = req.params.id;
-        UsersModel.findOne({_id: id}, function (err, Users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Users',
-                    error: err
-                });
-            }
-            if (!Users) {
-                return res.status(404).json({
-                    message: 'No such Users'
-                });
-            }
+      Users.name = req.body.name ? req.body.name : Users.name;      Users.hunger = req.body.hunger ? req.body.hunger : Users.hunger;      Users.thirst = req.body.thirst ? req.body.thirst : Users.thirst;      Users.fatigue = req.body.fatigue ? req.body.fatigue : Users.fatigue;      Users.awesomeness = req.body.awesomeness ? req.body.awesomeness : Users.awesomeness
+      Users.save(function (err, Users) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating Users.',
+            error: err
+          })
+        }
 
-            Users.name = req.body.name ? req.body.name : Users.name;			Users.hunger = req.body.hunger ? req.body.hunger : Users.hunger;			Users.thirst = req.body.thirst ? req.body.thirst : Users.thirst;			Users.fatigue = req.body.fatigue ? req.body.fatigue : Users.fatigue;			Users.awesomeness = req.body.awesomeness ? req.body.awesomeness : Users.awesomeness;			
-            Users.save(function (err, Users) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating Users.',
-                        error: err
-                    });
-                }
+        return res.json(Users)
+      })
+    })
+  },
 
-                return res.json(Users);
-            });
-        });
-    },
-
-    /**
-     * UsersController.remove()
-     */
-    remove: function (req, res) {
-        var id = req.params.id;
-        UsersModel.findByIdAndRemove(id, function (err, Users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the Users.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
-        });
-    }
-};
+  remove: function (req, res) {
+    var id = req.params.id
+    UsersModel.findByIdAndRemove(id, function (err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when deleting the Users.',
+          error: err
+        })
+      }
+      return res.status(204).json()
+    })
+  }
+}
